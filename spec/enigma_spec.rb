e11b2df3c434df343f2bcb784b7e73541c547b10
Michael Mattson <michael.p.mattson@gmail.com>
2021-08-06 09:41:19 -0600

    Add rest of iteration pattern tests

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe Enigma do
     it 'exists and has attributes' do
       enigma = Enigma.new
 
-      expect(something).to be_an_instance_of(expectation)
-      expect(something).to eq(expectation)
+      expect(enigma).to be_an_instance_of(Enigma)
+      # expect(something).to eq(expectation)
     end
   end
 
-  describe '#encrypt()' do
+  describe '#encrypt(text, key, date)' do
     xit 'encrypts a message with a key and date' do
       enigma = Enigma.new
 
@@ -42,7 +42,7 @@ RSpec.describe Enigma do
     end
   end
 
-  describe '#decrypt()' do
+  describe '#decrypt(ciphertext, key, date)' do
     xit 'decrypts a message with a key and date' do
       enigma = Enigma.new
 
@@ -63,6 +63,52 @@ RSpec.describe Enigma do
       #decrypt a message with a key (uses today's date)
       enigma.decrypt(encrypted[:encryption], "02715")
       #=> # decryption hash here
+    end
+  end
+
+  describe '#crack(ciphertext, date)' do
+    xit 'cracks an encryption with a date' do
+      enigma = Enigma.new
+
+      encryption = enigma.encrypt("hello world end", "08304", "291018")
+      #=>
+      #   {
+      #     encryption: "vjqtbeaweqihssi",
+      #     key: "08304",
+      #     date: "291018"
+      #   }
+
+      # crack an encryption with a date
+      crack = enigma.crack("vjqtbeaweqihssi", "291018")
+      #=>
+      #   {
+      #     decryption: "hello world end",
+      #     date: "291018",
+      #     key: "08304"
+      #   }
+
+      expect(crack[:decryption]).to eq(encryption[:encryption])
+      expect(crack[:date]).to eq(encryption[:date])
+      expect(crack[:key]).to eq(encryption[:key])
+    end
+
+    xit "cracks an encryption (uses today's date)" do
+      enigma = Enigma.new
+
+      encryption = enigma.encrypt("hello world end", "08304", "291018")
+
+      # crack an encryption (uses today's date)
+      crack = enigma.crack("vjqtbeaweqihssi")
+      #=>
+      #   {
+      #     decryption: "hello world end",
+      #     date: # todays date in the format DDMMYY,
+      #     key: # key used for encryption
+      #   }
+
+      expect(crack[:decryption]).to eq(encryption[:encryption])
+      # expect(crack[:date]).to eq(# todays date)
+      expect(crack[:key]).to eq(encryption[:key])
     end
   end
 end
